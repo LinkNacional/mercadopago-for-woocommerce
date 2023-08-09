2c4e273c49cdb797479ab59357a5a7d504b1b596
Emanuel Lopes <lopes.emanuel@linknacional.com>
2023-08-09 20:14:06 +0000

    ci: Update automated tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -18,7 +18,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin(): void {
-    require dirname( __DIR__ ) . '/woocommerce-mercadopago.php';
+    require dirname( __DIR__ ) . '/lkn-wc-mercadopago.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
